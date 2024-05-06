40c797f24e307f019e6326003265ffe95bacd18f
stefan11111 <stefan11111@shitposting.expert>
2024-05-06 15:24:56 +0300

    move a function to gdk

--- libgdk-3.c
+++ libgdk-3.c
@@ -4,3 +4,37 @@ void
 gdk_disable_multidevice (void)
 {
 }
+
+struct _GdkRGBA
+{
+  gdouble red;
+  gdouble green;
+  gdouble blue;
+  gdouble alpha;
+};
+
+typedef struct _GdkRGBA GdkRGBA;
+
+gchar *
+gdk_rgba_to_string (const GdkRGBA *rgba)
+{
+  if (rgba->alpha > 0.999)
+    {
+      return g_strdup_printf ("rgb(%d,%d,%d)",
+                              (int)(0.5 + CLAMP (rgba->red, 0., 1.) * 255.),
+                              (int)(0.5 + CLAMP (rgba->green, 0., 1.) * 255.),
+                              (int)(0.5 + CLAMP (rgba->blue, 0., 1.) * 255.));
+    }
+  else
+    {
+      gchar alpha[G_ASCII_DTOSTR_BUF_SIZE];
+
+      g_ascii_formatd (alpha, G_ASCII_DTOSTR_BUF_SIZE, "%g", CLAMP (rgba->alpha, 0, 1));
+
+      return g_strdup_printf ("rgba(%d,%d,%d,%s)",
+                              (int)(0.5 + CLAMP (rgba->red, 0., 1.) * 255.),
+                              (int)(0.5 + CLAMP (rgba->green, 0., 1.) * 255.),
+                              (int)(0.5 + CLAMP (rgba->blue, 0., 1.) * 255.),
+                              alpha);
+    }
+}
--- libgtk-3.c
+++ libgtk-3.c
@@ -222,30 +222,6 @@ struct _GdkRGBA
 
 typedef struct _GdkRGBA GdkRGBA;
 
-gchar *
-gdk_rgba_to_string (const GdkRGBA *rgba)
-{
-  if (rgba->alpha > 0.999)
-    {
-      return g_strdup_printf ("rgb(%d,%d,%d)",
-                              (int)(0.5 + CLAMP (rgba->red, 0., 1.) * 255.),
-                              (int)(0.5 + CLAMP (rgba->green, 0., 1.) * 255.),
-                              (int)(0.5 + CLAMP (rgba->blue, 0., 1.) * 255.));
-    }
-  else
-    {
-      gchar alpha[G_ASCII_DTOSTR_BUF_SIZE];
-
-      g_ascii_formatd (alpha, G_ASCII_DTOSTR_BUF_SIZE, "%g", CLAMP (rgba->alpha, 0, 1));
-
-      return g_strdup_printf ("rgba(%d,%d,%d,%s)",
-                              (int)(0.5 + CLAMP (rgba->red, 0., 1.) * 255.),
-                              (int)(0.5 + CLAMP (rgba->green, 0., 1.) * 255.),
-                              (int)(0.5 + CLAMP (rgba->blue, 0., 1.) * 255.),
-                              alpha);
-    }
-}
-
 #if 0
 static GdkRGBA GdkRGBA_from_GdkColor (GdkColor *color)
 {
