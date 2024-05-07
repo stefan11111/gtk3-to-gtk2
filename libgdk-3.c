7574e1b0bf14c0318edc5c404b7ce1ae12551c92
stefan11111 <stefan11111@shitposting.expert>
2024-05-07 16:42:49 +0300

    add some functions

--- libgdk-3.c
+++ libgdk-3.c
@@ -285,3 +285,315 @@ gdk_x11_window_get_xid (GdkWindow *window)
   return gdk_x11_drawable_get_xid (window);
 }
 #endif
+
+static cairo_format_t
+gdk_cairo_format_for_content (cairo_content_t content)
+{
+  switch (content)
+    {
+    case CAIRO_CONTENT_COLOR:
+      return CAIRO_FORMAT_RGB24;
+    case CAIRO_CONTENT_ALPHA:
+      return CAIRO_FORMAT_A8;
+    case CAIRO_CONTENT_COLOR_ALPHA:
+    default:
+      return CAIRO_FORMAT_ARGB32;
+    }
+}
+
+static cairo_surface_t *
+gdk_cairo_surface_coerce_to_image (cairo_surface_t *surface,
+                                   cairo_content_t  content,
+                                   int              src_x,
+                                   int              src_y,
+                                   int              width,
+                                   int              height)
+{
+  cairo_surface_t *copy;
+  cairo_t *cr;
+
+  copy = cairo_image_surface_create (gdk_cairo_format_for_content (content),
+                                     width,
+                                     height);
+
+  cr = cairo_create (copy);
+  cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_surface (cr, surface, -src_x, -src_y);
+  cairo_paint (cr);
+  cairo_destroy (cr);
+
+  return copy;
+}
+
+static void
+convert_alpha (guchar *dest_data,
+               int     dest_stride,
+               guchar *src_data,
+               int     src_stride,
+               int     src_x,
+               int     src_y,
+               int     width,
+               int     height)
+{
+  int y;
+
+  src_data += src_stride * src_y + src_x * 4;
+
+  for (y = 0; y < height; y++) {
+    guint32 *src = (guint32 *) src_data;
+
+    int x;
+
+    for (x = 0; x < width; x++) {
+      guint alpha = src[x] >> 24;
+
+      if (alpha == 0)
+        {
+          dest_data[x * 4 + 0] = 0;
+          dest_data[x * 4 + 1] = 0;
+          dest_data[x * 4 + 2] = 0;
+        }
+      else
+        {
+          dest_data[x * 4 + 0] = (((src[x] & 0xff0000) >> 16) * 255 + alpha / 2) / alpha;
+          dest_data[x * 4 + 1] = (((src[x] & 0x00ff00) >>  8) * 255 + alpha / 2) / alpha;
+          dest_data[x * 4 + 2] = (((src[x] & 0x0000ff) >>  0) * 255 + alpha / 2) / alpha;
+        }
+      dest_data[x * 4 + 3] = alpha;
+    }
+
+    src_data += src_stride;
+    dest_data += dest_stride;
+  }
+}
+
+static void
+convert_no_alpha (guchar *dest_data,
+                  int     dest_stride,
+                  guchar *src_data,
+                  int     src_stride,
+                  int     src_x,
+                  int     src_y,
+                  int     width,
+                  int     height)
+{
+  int y;
+
+  src_data += src_stride * src_y + src_x * 4;
+
+  for (y = 0; y < height; y++) {
+    guint32 *src = (guint32 *) src_data;
+    int x;
+    for (x = 0; x < width; x++) {
+      dest_data[x * 3 + 0] = src[x] >> 16;
+      dest_data[x * 3 + 1] = src[x] >>  8;
+      dest_data[x * 3 + 2] = src[x];
+    }
+
+    src_data += src_stride;
+    dest_data += dest_stride;
+  }
+}
+
+GdkPixbuf *
+gdk_pixbuf_get_from_surface  (cairo_surface_t *surface,
+                              gint             src_x,
+                              gint             src_y,
+                              gint             width,
+                              gint             height)
+{
+  cairo_content_t content;
+  GdkPixbuf *dest;
+
+  /* General sanity checks */
+  g_return_val_if_fail (surface != NULL, NULL);
+  g_return_val_if_fail (width > 0 && height > 0, NULL);
+
+  content = cairo_surface_get_content (surface) | CAIRO_CONTENT_COLOR;
+  dest = gdk_pixbuf_new (GDK_COLORSPACE_RGB,
+                         !!(content & CAIRO_CONTENT_ALPHA),
+                         8,
+                         width, height);
+
+  if (cairo_surface_get_type (surface) == CAIRO_SURFACE_TYPE_IMAGE &&
+      cairo_image_surface_get_format (surface) == gdk_cairo_format_for_content (content))
+    surface = cairo_surface_reference (surface);
+  else
+    {
+      surface = gdk_cairo_surface_coerce_to_image (surface, content,
+                                                   src_x, src_y,
+                                                   width, height);
+      src_x = 0;
+      src_y = 0;
+    }
+  cairo_surface_flush (surface);
+  if (cairo_surface_status (surface) || dest == NULL)
+    {
+      cairo_surface_destroy (surface);
+      g_clear_object (&dest);
+      return NULL;
+    }
+
+  if (gdk_pixbuf_get_has_alpha (dest))
+    convert_alpha (gdk_pixbuf_get_pixels (dest),
+                   gdk_pixbuf_get_rowstride (dest),
+                   cairo_image_surface_get_data (surface),
+                   cairo_image_surface_get_stride (surface),
+                   src_x, src_y,
+                   width, height);
+  else
+    convert_no_alpha (gdk_pixbuf_get_pixels (dest),
+                      gdk_pixbuf_get_rowstride (dest),
+                      cairo_image_surface_get_data (surface),
+                      cairo_image_surface_get_stride (surface),
+                      src_x, src_y,
+                      width, height);
+
+  cairo_surface_destroy (surface);
+  return dest;
+}
+
+struct _GdkWindowPaint
+{
+  GdkRegion *region;
+  GdkPixmap *pixmap;
+  gint x_offset;
+  gint y_offset;
+  cairo_surface_t *surface;
+  guint uses_implicit : 1;
+  guint flushed : 1;
+  guint32 region_tag;
+};
+
+static inline void
+gdk_window_cairo_surface_destroy (void *data)
+{
+#if 0 /* wastes memory and cpu cycles */
+  struct _GdkWindowObjectReal *private = (struct _GdkWindowObjectReal*) data;
+
+  private->cairo_surface = NULL;
+  ((struct _GdkWindowObjectReal*)private->impl_window)->outstanding_surfaces--;
+#endif /* lean, fast, unreadable */
+  ((struct _GdkWindowObjectReal*)data)->cairo_surface = NULL;
+  ((struct _GdkWindowObjectReal*)((struct _GdkWindowObjectReal*)data)->impl_window)->outstanding_surfaces--;
+}
+
+static inline GdkDrawable *
+_gdk_drawable_get_source_drawable (GdkDrawable *drawable)
+{
+  g_return_val_if_fail (GDK_IS_DRAWABLE (drawable), NULL);
+
+  if (GDK_DRAWABLE_GET_CLASS (drawable)->get_source_drawable)
+    return GDK_DRAWABLE_GET_CLASS (drawable)->get_source_drawable (drawable);
+
+  return drawable;
+}
+
+static cairo_surface_t *
+gdk_window_ref_cairo_surface (GdkDrawable *drawable)
+{
+  struct _GdkWindowObjectReal *private = (struct _GdkWindowObjectReal*) drawable;
+  cairo_surface_t *surface;
+
+  if (private->paint_stack)
+    {
+      GdkWindowPaint *paint = private->paint_stack->data;
+
+      surface = paint->surface;
+      cairo_surface_reference (surface);
+    }
+  else
+    {
+
+      /* This will be drawing directly to the window, so flush implicit paint */
+      gdk_window_flush ((GdkWindow *)drawable);
+
+      if (!private->cairo_surface)
+        {
+          int width, height;
+
+          gdk_drawable_get_size ((GdkWindow *) private->impl_window,
+                                 &width, &height);
+
+          GdkDrawable *source = _gdk_drawable_get_source_drawable(drawable);
+
+          private->cairo_surface = GDK_DRAWABLE_GET_CLASS (source)->create_cairo_surface (source, width, height);
+          if (private->cairo_surface)
+            {
+              ((struct _GdkWindowObjectReal*)private->impl_window)->outstanding_surfaces++;
+
+              cairo_surface_set_device_offset (private->cairo_surface,
+                                               private->abs_x,
+                                               private->abs_y);
+
+              static const cairo_user_data_key_t gdk_window_cairo_key;
+
+              cairo_surface_set_user_data (private->cairo_surface, &gdk_window_cairo_key,
+                                           drawable, gdk_window_cairo_surface_destroy);
+            }
+        }
+      else
+        cairo_surface_reference (private->cairo_surface);
+
+      surface = private->cairo_surface;
+    }
+
+  return surface;
+}
+
+GdkPixbuf *
+gdk_pixbuf_get_from_window (GdkWindow *src,
+                            gint       src_x,
+                            gint       src_y,
+                            gint       width,
+                            gint       height)
+{
+  cairo_surface_t *copy;
+  cairo_t *cr;
+  GdkPixbuf *dest;
+
+  g_return_val_if_fail (GDK_IS_WINDOW (src), NULL);
+  g_return_val_if_fail (gdk_window_is_viewable (src), NULL);
+
+  cairo_surface_t *surface = gdk_window_ref_cairo_surface (src);
+  int scale = gdk_window_get_scale_factor (src);
+
+  /* We do not know what happened to this surface outside of GDK.
+   * Especially for foreign windows, they will have been modified
+   * by external applications.
+   *
+   * So be on the safe side and:
+   * - flush the Cairo state
+   * - mark the surface as dirty, in case the GdkWindow was
+   *   created from a foreign X11 surface
+   *
+   * THE ORDER IS IMPORTANT. DO NOT CHANGE IT.
+   *
+   * For reference, see:
+   * - https://bugzilla.gnome.org/show_bug.cgi?id=754952
+   * - https://gitlab.gnome.org/GNOME/gtk/-/issues/4456
+   * - https://gitlab.gnome.org/GNOME/gtk/-/issues/5691
+   */
+  cairo_surface_flush (surface);
+  cairo_surface_mark_dirty (surface);
+
+  if (cairo_surface_get_content (surface) & CAIRO_CONTENT_ALPHA)
+    copy = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, width * scale, height * scale);
+  else
+    copy = cairo_image_surface_create (CAIRO_FORMAT_RGB24, width * scale, height * scale);
+
+  cairo_surface_set_device_scale (copy, scale, scale);
+
+  cr = cairo_create (copy);
+  cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_surface (cr, surface, -src_x, -src_y);
+  cairo_paint (cr);
+  cairo_destroy (cr);
+
+  dest = gdk_pixbuf_get_from_surface (copy, 0, 0, width * scale, height * scale);
+
+  cairo_surface_destroy (copy);
+  cairo_surface_destroy (surface);
+
+  return dest;
+}
